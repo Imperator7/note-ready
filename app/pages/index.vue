507d945e72a314c5400e0832a263fa27a1c668c8
Imperator7 <140708045+Imperator7@users.noreply.github.com>
2025-12-29 16:29:06 +0700

    refactor: move out the NoteCreateform from the index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -3,24 +3,11 @@ definePageMeta({
   middleware: 'auth',
 })
 
-const { user, loggedIn } = useUserSession()
+const { loggedIn } = useUserSession()
 
-const newNote = ref<string>('')
-const category = ref<string>('work')
 const sortBy = ref<string>('Newest first')
 
-const { fetchNotes, createNote } = useNote()
-
-const addNote = () => {
-  createNote({
-    note: newNote.value,
-    author: user.value?.name,
-    category: category.value,
-  })
-
-  newNote.value = ''
-  category.value = 'work'
-}
+const { fetchNotes } = useNote()
 
 onMounted(() => {
   fetchNotes()
@@ -43,34 +30,7 @@ onMounted(() => {
         <option>Oldest first</option>
       </select>
     </div>
-    <form v-show="loggedIn" class="flex gap-4" @submit.prevent="addNote">
-      <input
-        type="text"
-        v-model="newNote"
-        class="border p-2 rounded"
-        placeholder="Type here..."
-        required
-      />
-      <select
-        name="category"
-        class="border rounded px-1 cursor-pointer"
-        v-model="category"
-      >
-        <option>work</option>
-        <option>study</option>
-        <option>rest</option>
-        <option>quote</option>
-        <option>chore</option>
-      </select>
-      <div class="flex gap-2 items-center">
-        <button
-          class="py-2 px-4 bg-sky-700 text-white font-medium rounded cursor-pointer flex items-center hover:-translate-y-0.5 transition duration-75 ease active:translate-y-1 active:bg-sky-500"
-          type="submit"
-        >
-          Create
-        </button>
-      </div>
-    </form>
+    <NoteCreateForm />
     <NoteList :sort-by="sortBy" />
   </div>
 </template>
