2f6f8736213d0b6afadd6c5731a53a31ad3cc445
Imperator7 <140708045+Imperator7@users.noreply.github.com>
2026-01-13 01:12:24 +0700

    feat: implement pagination by page query on get note endpoint

--- server/api/notes.get.ts
+++ server/api/notes.get.ts
@@ -1,5 +1,22 @@
-export default defineEventHandler((event) => {
-  const { getAll: getNotes } = useNoteService()
+import z from 'zod'
+
+export default defineEventHandler(async (event) => {
+  const { getAll: getNotes, getByPage, getMeta } = useNoteService()
+
+  const querySchema = z.object({
+    page: z.coerce.number().int().positive().optional(),
+  })
+
+  const { page } = await getValidatedQuery(event, querySchema.parse)
+
+  if (page) {
+    return {
+      success: true,
+      status: 200,
+      data: getByPage(page),
+      meta: getMeta(page),
+    }
+  }
 
   return { success: true, status: 200, data: getNotes() }
 })
